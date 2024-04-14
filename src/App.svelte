b1191c64c8062ec6f82d01078092c6d70b695ff2
Peter Kipping <pkipping@yahoo.com>
2024-04-14 18:36:46 -0400

    added if else logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,38 @@
 <script>
+  import Nested from "./components/Nested.svelte";
 
-	import Nested from './components/Nested.svelte';
-
-	let name = 'Svelte';
-  let src = './assets/svelte.svg';
-  let name2 = 'Rick Ashley';
+  let name = "Svelte";
+  let src = "./assets/svelte.svg";
+  let name2 = "Rick Ashley";
 
   let count = 0;
   $: doubled = count * 2;
 
-	function increment() {
-		count += 1;
-	}
-
+  function increment() {
+    count += 1;
+  }
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
-<img {src} alt="{name2} dances."/>
+<img {src} alt="{name2} dances." />
 <p>This is a paragraph.</p>
-<Nested answer={42}/>
+<Nested answer={42} />
 <button on:click={increment}>
-	Clicked {count}
-	{count === 1 ? 'time' : 'times'}
+  Clicked {count}
+  {count === 1 ? "time" : "times"}
 </button>
 <p>{count} doubled is {doubled}</p>
 
+{#if count > 10}
+  <p>{count} is greater than 10</p>
+{:else}
+  <p>{count} is between 0 and 10</p>
+{/if}
 
 <style>
-	p {
-		color: goldenrod;
-		font-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
-	}
+  p {
+    color: goldenrod;
+    font-family: "Comic Sans MS", cursive;
+    font-size: 2em;
+  }
 </style>
